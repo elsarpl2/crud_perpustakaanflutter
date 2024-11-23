f394d7213a5b78d24ec12e60c45c92cdd50c1870
elsarpl2 <elsada.xpplg2@gmail.com>
2024-11-23 09:05:44 +0700

    commit pertama

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,11 @@
 // Import library utama Flutter untuk membuat UI.
-import 'package:flutter/material.dart';
+import 'package:crud_perpustakaan/insert.dart'; // Mengimport halaman untuk menambah buku.
+import 'package:flutter/material.dart'; // Mengimport library Flutter untuk widget UI.
 
 // Import library Supabase untuk mengintegrasikan backend menggunakan Supabase.
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart'; // Mengimport library Supabase untuk menghubungkan dengan database.
 
-// Membuat kelas BookListPage yang merupakan StatefulWidget, 
+// Membuat kelas BookListPage yang merupakan StatefulWidget,
 // digunakan untuk menampilkan daftar buku.
 class BookListPage extends StatefulWidget {
   const BookListPage({super.key}); // Konstruktor dengan kunci untuk widget ini.
@@ -28,8 +29,8 @@ class _BookListPageState extends State<BookListPage> {
   // Fungsi untuk mengambil data buku dari Supabase
   Future<void> fetchBooks() async {
     final response = await Supabase.instance.client
-    .from('buku') // Mengambil data dari tabel 'buku'.
-    .select(); // Query untuk mengambil semua data.
+        .from('buku') // Mengambil data dari tabel 'buku'.
+        .select(); // Query untuk mengambil semua data.
 
     setState(() {
       buku = List<Map<String, dynamic>>.from(response); // Menyimpan hasil query ke dalam list buku.
@@ -52,85 +53,108 @@ class _BookListPageState extends State<BookListPage> {
       ),
       // Menampilkan data buku jika ada, jika kosong akan menampilkan loading.
       body: buku.isEmpty
-      ? const Center(child: CircularProgressIndicator()) // Menampilkan indikator loading saat data belum ada.
-       : ListView.builder( // Menggunakan ListView.builder untuk menampilkan daftar buku.
-         itemCount: buku.length, // Menentukan jumlah item berdasarkan panjang data buku.
-         itemBuilder: (context, index) {
-          final book = buku[index]; // Mengambil data buku berdasarkan index.
-          return ListTile(
-            title: Text(book['judul'] ?? 'No Title', style: const TextStyle( // Menampilkan judul buku atau 'No Title' jika kosong.
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-            )),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start, // Menyusun elemen subtitle secara vertikal.
-              children: [
-                Text(book['penulis'] ?? 'No Author', style: TextStyle( // Menampilkan penulis atau 'No Author' jika kosong.
-                  fontStyle: FontStyle.italic,
-                  fontSize: 14.0,
-                ),),
-                Text(book['deskripsi'] ?? 'No Description', style: TextStyle( // Menampilkan deskripsi atau 'No Description' jika kosong.
-                  fontSize: 12.0,
-                ),),
-              ],
+          ? const Center(child: CircularProgressIndicator()) // Menampilkan indikator loading saat data belum ada.
+          : ListView.builder( // Menggunakan ListView.builder untuk menampilkan daftar buku.
+              itemCount: buku.length, // Menentukan jumlah item berdasarkan panjang data buku.
+              itemBuilder: (context, index) {
+                final book = buku[index]; // Mengambil data buku berdasarkan index.
+                return ListTile(
+                  title: Text(
+                    book['judul'] ?? 'No Title', // Menampilkan judul buku atau 'No Title' jika kosong.
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start, // Menyusun elemen subtitle secara vertikal.
+                    children: [
+                      Text(
+                        book['penulis'] ?? 'No Author', // Menampilkan penulis atau 'No Author' jika kosong.
+                        style: TextStyle(
+                          fontStyle: FontStyle.italic,
+                          fontSize: 14.0,
+                        ),
+                      ),
+                      Text(
+                        book['deskripsi'] ?? 'No Description', // Menampilkan deskripsi atau 'No Description' jika kosong.
+                        style: TextStyle(
+                          fontSize: 12.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min, // Menyusun tombol edit dan delete secara horizontal.
+                    children: [
+                      // Tombol untuk mengedit buku.
+                      IconButton(
+                        icon: const Icon(Icons.edit, color: Colors.blue), // Ikon edit.
+                        onPressed: () {
+                          // Arahkan ke halaman EditBooks untuk mengedit buku (dalam kode ini tombol belum berfungsi).
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(
+                          //     builder: (context) => EditBookPage(book: book),
+                          //   ),
+                          // ).then((_) {
+                          //   fetchBooks(); // Refresh data setelah kembali dari halaman edit.
+                          // });
+                        },
+                      ),
+                      // Tombol untuk menghapus buku.
+                      IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.red), // Ikon delete.
+                        onPressed: () {
+                          // Menampilkan dialog konfirmasi untuk menghapus buku.
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('Delete Book'), // Judul dialog.
+                                content: const Text('Are you sure you want to delete this book?'), // Pesan konfirmasi penghapusan.
+                                actions: [
+                                  // Tombol untuk membatalkan penghapusan.
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop(); // Menutup dialog jika tombol 'Cancel' ditekan.
+                                    },
+                                    child: const Text('Cancel'),
+                                  ),
+                                  // Tombol untuk mengonfirmasi penghapusan.
+                                  TextButton(
+                                    onPressed: () async {
+                                      // Fungsi penghapusan buku (belum diimplementasikan).
+                                      // await deletedBook(book['id']);
+                                      Navigator.of(context).pop(); // Menutup dialog setelah penghapusan.
+                                    },
+                                    child: const Text('Delete'),
+                                  )
+                                ],
+                              );
+                            },
+                          );
+                        },
+                      )
+                    ],
+                  ),
+                );
+              },
             ),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min, // Menyusun tombol edit dan delete secara horizontal.
-              children: [
-                // Tombol untuk mengedit buku.
-                IconButton(
-                  icon: const Icon(Icons.edit, color: Colors.blue), // Ikon edit.
-                  onPressed: () {
-                    // Arahkan ke halaman EditBooks untuk mengedit buku (dalam kode ini tombol belum berfungsi).
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //     builder: (context) => EditBookPage(book: book),
-                    //   ),
-                    // ).then((_) {
-                    //   fetchBooks(); // Refresh data setelah kembali dari halaman edit.
-                    // });
-                  },
-                ),
-                // Tombol untuk menghapus buku.
-                IconButton(
-                  icon: const Icon(Icons.delete, color: Colors.red), // Ikon delete.
-                  onPressed: () {
-                    // Menampilkan dialog konfirmasi untuk menghapus buku.
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: const Text('Delete Book'), // Judul dialog.
-                          content: const Text('Are you sure you want to delete this book?'), // Pesan konfirmasi penghapusan.
-                          actions: [
-                            // Tombol untuk membatalkan penghapusan.
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop(); // Menutup dialog jika tombol 'Cancel' ditekan.
-                              },
-                              child: const Text('Cancel'),
-                            ),
-                            // Tombol untuk mengonfirmasi penghapusan.
-                            TextButton(
-                              onPressed: () async {
-                                // Fungsi penghapusan buku (belum diimplementasikan).
-                                // await deletedBook(book['id']);
-                                Navigator.of(context).pop(); // Menutup dialog setelah penghapusan.
-                              },
-                              child: const Text('Delete'),
-                            )
-                          ],
-                        );
-                      },
-                    );
-                  },
-                )
-              ],
+      // FloatingActionButton untuk menambahkan buku baru.
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigasi ke halaman AddBookPage untuk menambah buku baru.
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddBookPage(),
             ),
           );
-         },
-       ),
+        },
+        child: const Icon(Icons.add), // Ikon untuk menambah buku.
+        tooltip: 'Add Book', // Tooltip untuk tombol tambah buku.
+      ),
     );
   }
 }
